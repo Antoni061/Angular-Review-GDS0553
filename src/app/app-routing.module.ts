57daf492cf5e6f01a9a6d2d1e4b905a015baaa6e
Antoni061 <ezapatero939@gmail.com>
2024-02-16 15:17:37 -0600

    version 5

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegisterComponent } from './componentes/register/register.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { authGuard } from './guardia/auth.guard';
+const routes: Routes = [
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {path: 'register', component: RegisterComponent},
+  {path: 'home', component: HomeComponent,canActivate:[authGuard]},
+  {path: '',redirectTo: 'home',pathMatch: 'full'}
+
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
